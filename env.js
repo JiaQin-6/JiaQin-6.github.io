d6145f5e26f1f1609a19777c6f5b8508818617b3
嘉嘉 <1723470065@qq.com>
2022-09-06 16:28:26 +0800

    20220906

--- env.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-06-22 15:51:34
- * @LastEditTime: 2022-06-27 11:59:10
- * @LastEditors: 嘉嘉 1723470065@qq.com
- * @Description: In User Settings Edit
- * @FilePath: \TSc:\Users\Shinelon\Desktop\company-product\dtX-consumer-frontend\public\env.js
- */
-window.env='dev'
